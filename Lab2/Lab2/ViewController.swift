a2c8d7fa03aac22ec9b480d4e224c9764615f5bf
Naitik59 <123869903+Naitik59@users.noreply.github.com>
2024-05-14 17:55:54 -0400

    IBAction and IBOutlet added

--- Lab2/Lab2/ViewController.swift
+++ Lab2/Lab2/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    /*
+     sender tags for different numbers
+     0 - 0
+     1 - 1
+     2 - 2 and so on
+     */
+    @IBAction func numberTapped(_ sender: UIButton) {
+        print(sender.titleLabel?.text ?? "")
+    }
+    
+    /*
+     sender tags for different math signs
+     0 - .
+     1 - =
+     2 - +
+     3 - -(minus)
+     4 - x
+     5 - /
+     6 - x^2
+     7 - +/-
+     8 - AC
+     */
+    @IBAction func mathematicalSignTapped(_ sender: UIButton) {
+        print(sender.titleLabel?.text ?? "")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
