d86240f7387ebd6410f1ff092ebd4e5794221c51
Naitik59 <123869903+Naitik59@users.noreply.github.com>
2024-05-14 17:18:53 -0400

    LAB 2 Initial Commit

---
+++ Lab2/Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by Naitik Ratilal Patel on 13/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
